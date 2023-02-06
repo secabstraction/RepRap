af9dcb4086cdfa7770817583be76034ba0447a71
secabstraction <jesse@secabstraction.com>
2023-02-05 21:40:01 -0800

    initial commit of dual-z on marlin 2.1.2 (v2.2.1 creality board)

---
+++ Firmware/Creality/CR10_S5/Marlin/src/pins/ramps/pins_RAMPS_CREALITY.h
@@ -0,0 +1,43 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#if HOTENDS > 1 || E_STEPPERS > 1
+  #error "Creality RAMPS supports up to 1 hotends / E steppers."
+#endif
+
+#define BOARD_INFO_NAME "Creality v2.2.1 (RAMPS 1.3)"
+
+// use E1 stepper as Z2 to enable G34
+#define E1_STEP_PIN                         -1
+#define E1_DIR_PIN                          -1
+#define E1_ENABLE_PIN                       -1
+#define E1_CS_PIN                           -1
+
+#define Z2_STEP_PIN                         36
+#define Z2_DIR_PIN                          34
+#define Z2_ENABLE_PIN                       30
+#define Z2_CS_PIN                           44
+
+#define SD_DETECT_PIN                       49
+
+#include "pins_RAMPS.h"
